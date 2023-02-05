701c1fe19f7c351ee7315f6fc813cfe0c3963505
Lucas Turossi <lucasturossi@protonmail.com>
2023-02-04 21:40:55 -0300

    Add files via upload

---
+++ Port_Scan.ps1
@@ -0,0 +1,15 @@
+﻿param($ip)
+
+if(!$ip ){
+    echo "DESEC SECURITY - PORTSCAN"
+    echo "Developed BY N0KK"
+    echo ".\portscan.ps1 192.168.0.1"
+}
+else{
+    foreach ($port in 1..65535){
+    If (Test-NetConnection $ip -port $port -WarningAction SilentlyContinue -InformationLevel Quiet) {
+        echo “Porta $port Aberta”}
+        }
+    else {
+        echo “Porta $port fechada”} 
+}
---
+++ Port_Scan_Top_Ports.ps1
@@ -0,0 +1,21 @@
+﻿param($ip)
+
+if(!$ip ){
+    echo "DESEC SECURITY - PORTSCAN"
+    echo "Developed BY N0KK"
+    echo ".\portscan.ps1 192.168.0.1"
+}
+else{
+    $topports = 21, 22, 25, 3306, 80, 444 
+    try {foreach ($port in $topports){
+      If (Test-NetConnection $ip -port $port -WarningAction SilentlyContinue -InformationLevel Quiet) {
+        echo “Porta $port Aberta”}
+        }
+    else {
+        echo “Porta $port fechada”} 
+
+        }
+    catch {}
+
+}
+       
