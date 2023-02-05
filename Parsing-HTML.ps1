76b7b6467caea32b2f14223effd7900157539018
Lucas Turossi <lucasturossi@protonmail.com>
2023-02-05 19:16:27 -0300

    Add files via upload

---
+++ Parsing-HTML.ps1
@@ -0,0 +1,14 @@
+﻿$domain = Read-Host "Digite o domínio: "
+$web = Invoke-WebRequest -uri "$domain" -Method Options 
+echo "O server roda: "
+echo " "
+$web.headers.server
+echo ""
+echo "O server aceita os métodos: "
+echo " "
+$web.headers.allow
+echo " "
+echo "Links encontrados: "
+echo " "
+$links = Invoke-WebRequest -uri "$domain"
+$links.links.href | Select-String "http://"
